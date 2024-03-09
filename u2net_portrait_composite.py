4863e37de1b257bbf462b27240d9b4b1dccc0012
Peter Rasmussen <prasmus3@jh.edu>
2024-03-09 13:34:12 -0500

    Remove .gitattributes b/c git lfs not allowed for forks and update u2net_portrait_composite

--- u2net_portrait_composite.py
+++ u2net_portrait_composite.py
@@ -1,141 +1,190 @@
+# Standard library imports
+import argparse
+import glob
 import os
+from pathlib import Path
+
+# Third party imports
+import numpy as np
 from PIL import Image
 from skimage import io, transform
 from skimage.filters import gaussian
 import torch
-import torchvision
 from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms#, utils
-# import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
-import numpy as np
-from PIL import Image
-import glob
-
+# Local imports
 from data_loader import RescaleT
 from data_loader import ToTensor
 from data_loader import ToTensorLab
 from data_loader import SalObjDataset
+from model import U2NET  # full size version 173.6 MB
 
-from model import U2NET # full size version 173.6 MB
-from model import U2NETP # small version u2net 4.7 MB
 
-import argparse
+SRC_DIR = Path("./test_data/test_portrait_images/your_portrait_im")
+MODEL_DIR = Path("./saved_models/u2net_portrait/u2net_portrait.pth")
+DST_DIR = Path("./test_data/test_portrait_images/your_portrait_results")
 
 # normalize the predicted SOD probability map
-def normPRED(d):
+def norm_pred(d: torch.Tensor) -> torch.Tensor:
+    """
+    Normalize the input tensor.
+    Arguments:
+    - d: The input tensor to be normalized.
+    Returns:
+    - dn: The normalized tensor.
+    """
     ma = torch.max(d)
     mi = torch.min(d)
 
-    dn = (d-mi)/(ma-mi)
+    dn = (d - mi) / (ma - mi)
 
     return dn
 
-def save_output(image_name,pred,d_dir,sigma=2,alpha=0.5):
+
+def save_output(image_name, pred, d_dir, sigma=2, alpha=0.5):
 
     predict = pred
     predict = predict.squeeze()
     predict_np = predict.cpu().data.numpy()
 
     image = io.imread(image_name)
-    pd = transform.resize(predict_np,image.shape[0:2],order=2)
-    pd = pd/(np.amax(pd)+1e-8)*255
-    pd = pd[:,:,np.newaxis]
+    pd = transform.resize(predict_np, image.shape[0:2], order=2)
+    pd = pd / (np.amax(pd) + 1e-8) * 255
+    pd = pd[:, :, np.newaxis]
 
     print(image.shape)
     print(pd.shape)
 
     ## fuse the orignal portrait image and the portraits into one composite image
     ## 1. use gaussian filter to blur the orginal image
-    sigma=sigma
+    sigma = sigma
     image = gaussian(image, sigma=sigma, preserve_range=True)
 
     ## 2. fuse these orignal image and the portrait with certain weight: alpha
     alpha = alpha
-    im_comp = image*alpha+pd*(1-alpha)
+    im_comp = image * alpha + pd * (1 - alpha)
     im = Image.fromarray(np.round(im_comp).astype(np.uint8))
     print(im_comp.shape)
     img_name = image_name.split(os.sep)[-1]
     aaa = img_name.split(".")
     bbb = aaa[0:-1]
     imidx = bbb[0]
-    for i in range(1,len(bbb)):
+    for i in range(1, len(bbb)):
         imidx = imidx + "." + bbb[i]
-    dst = d_dir+'/'+imidx+'_sigma_' + str(sigma) + '_alpha_' + str(alpha) + '_composite.png'
+    dst = (
+        d_dir
+        + "/"
+        + imidx
+        + "_sigma_"
+        + str(sigma)
+        + "_alpha_"
+        + str(alpha)
+        + "_composite.png"
+    )
     im.save(dst)
+
+
+def fuse_input_and_portrait(
+    input_image_src: Path,
+    portrait: torch.Tensor,
+    sigma: float = 2,
+    alpha: float = 0.5,
+) -> Image:
+    """
+    Fuse the original image and the portrait image into one composite image.
+
+    Arguments:
+        image: The path to the original image.
+        portrait: The portrait image.
+        sigma: The standard deviation of the Gaussian filter applied to the original image.
+        alpha: The weight of the original image in the composite image.
+    Returns:
+        The composite image.
+    """
+    # Prepare original image for fusion.
+    image = io.imread(input_image_src)
+    image = gaussian(image, sigma=sigma, preserve_range=True)
+    # Prepare portrait for fusion.
+    portrait = portrait.squeeze().cpu().data.numpy()
+    portrait = transform.resize(portrait, image.shape[0:2], order=2)
+    portrait = portrait / (np.amax(portrait) + 1e-8) * 255
+    portrait = portrait[:, :, np.newaxis]
+    # Fuse original image and portrait using weight alpha
+    composite_image = image * alpha + portrait * (1 - alpha)
+    return Image.fromarray(np.round(composite_image).astype(np.uint8))
+
 
 def main():
 
     parser = argparse.ArgumentParser(description="image and portrait composite")
-    parser.add_argument('-s',action='store',dest='sigma')
-    parser.add_argument('-a',action='store',dest='alpha')
+    parser.add_argument("--src_dir", "-i", type=Path, default=SRC_DIR, help="Path to the input image directory.")
+    parser.add_argument("--model_dir", "-j", type=Path, default=MODEL_DIR, help="Path to the model directory.")
+    parser.add_argument("--dst_dir", "-o", type=Path, default=DST_DIR, help="Path to the output image directory.")
+    parser.add_argument("--sigma", "-s", type=float, default=2.0, help="Sigma value for gaussian filter.")
+    parser.add_argument("--alpha", "-a", type=float, default=0.5, help="Alpha value for image and portrait fusion.")
+    parser.add_argument("--save_all", "-sa", action="store_true", help="Save all the results, not just best ones.")
     args = parser.parse_args()
-    print(args.sigma)
-    print(args.alpha)
-    print("--------------------")
+    
+    print("Arguments:")
+    for k, v in vars(args).items():
+        print(f"\t{k}: {v}")
 
-    # --------- 1. get image path and name ---------
-    model_name='u2net_portrait'#u2netp
+    # Fetch input image paths and make output directory
+    srcs = list(args.src_dir.iterdir())
+    args.dst_dir.mkdir(exist_ok=True, parents=True)
+    print(f"Number of images: {len(srcs)}")
 
+    # Instantiate dataset and dataloader
+    test_salobj_dataset = SalObjDataset(
+        img_name_list=srcs,
+        lbl_name_list=[],
+        transform=transforms.Compose([RescaleT(512), ToTensorLab(flag=0)]),
+    )
+    test_salobj_dataloader = DataLoader(
+        test_salobj_dataset, batch_size=1, shuffle=False, num_workers=1
+    )
 
-    image_dir = './test_data/test_portrait_images/your_portrait_im'
-    prediction_dir = './test_data/test_portrait_images/your_portrait_results'
-    if(not os.path.exists(prediction_dir)):
-        os.mkdir(prediction_dir)
-
-    model_dir = './saved_models/u2net_portrait/u2net_portrait.pth'
-
-    img_name_list = glob.glob(image_dir+'/*')
-    print("Number of images: ", len(img_name_list))
-
-    # --------- 2. dataloader ---------
-    #1. dataloader
-    test_salobj_dataset = SalObjDataset(img_name_list = img_name_list,
-                                        lbl_name_list = [],
-                                        transform=transforms.Compose([RescaleT(512),
-                                                                      ToTensorLab(flag=0)])
-                                        )
-    test_salobj_dataloader = DataLoader(test_salobj_dataset,
-                                        batch_size=1,
-                                        shuffle=False,
-                                        num_workers=1)
-
-    # --------- 3. model define ---------
-
+    # Load model and set to evaluation
     print("...load U2NET---173.6 MB")
-    net = U2NET(3,1)
-
-    net.load_state_dict(torch.load(model_dir))
+    net = U2NET(3, 1)
+    net.load_state_dict(torch.load(args.model_dir))
     if torch.cuda.is_available():
         net.cuda()
     net.eval()
 
-    # --------- 4. inference for each image ---------
+    # Run inference for each image
     for i_test, data_test in enumerate(test_salobj_dataloader):
-
-        print("inferencing:",img_name_list[i_test].split(os.sep)[-1])
-
-        inputs_test = data_test['image']
+        inputs_test = data_test["image"]
         inputs_test = inputs_test.type(torch.FloatTensor)
-
         if torch.cuda.is_available():
             inputs_test = Variable(inputs_test.cuda())
         else:
             inputs_test = Variable(inputs_test)
 
-        d1,d2,d3,d4,d5,d6,d7= net(inputs_test)
+        # Infer        
+        preds = net(inputs_test)
+        # Iterate over each prediction, fuse input with prediction, and save composite output
+        for ix, pred in enumerate(preds, start=1):
+            pred = 1.0 - pred[:, 0, :, :]
+            pred = norm_pred(pred)
+            
+            # fuse the original image and the portrait
+            composite_image = fuse_input_and_portrait(
+                srcs[i_test],
+                pred,
+                sigma=float(args.sigma),
+                alpha=float(args.alpha),
+            )
+            # save results to test_results folder
+            fn = f"{srcs[i_test].stem}_d{ix}_a{args.alpha}_s{args.sigma}_composite.png"
+            dst = args.dst_dir / fn
+            print(f"Saving composite image to {dst}.")
+            composite_image.save(str(dst))
+            if not args.save_all:
+                break
 
-        # normalization
-        pred = 1.0 - d1[:,0,:,:]
-        pred = normPRED(pred)
-
-        # save results to test_results folder
-        save_output(img_name_list[i_test],pred,prediction_dir,sigma=float(args.sigma),alpha=float(args.alpha))
-
-        del d1,d2,d3,d4,d5,d6,d7
 
 if __name__ == "__main__":
     main()
